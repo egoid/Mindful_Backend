d9e1237a1b025c447545390d4b147ccfb711c9f3
Samuel Aronoff <saronoff@gmail.com>
2016-08-13 13:48:22 -0700

    Adding SELECT checks first for company, role, type

--- routes/prod/job.js
+++ routes/prod/job.js
@@ -92,19 +92,6 @@ const LABEL_TO_RADIUS = {
   drive: 10
 };
 
-function _extract_job_def(req) {
-  return {
-    title: req.body.title,
-    location: req.body.location || null,
-    pay_rate_min: req.body.pay_rate_min || null,
-    pay_rate_max: req.body.pay_rate_max || null,
-    min_gpa: req.body.min_gpa || null,
-    description: req.body.description || null,
-    external_url: req.body.external_url || null,
-    posted_at: req.body.posted_at || null,
-    takedown_at: req.body.takedown_at || null
-  };
-}
 function _create_company(company_def, conn, all_done) {
   let industry_id;
   let company_id;
@@ -124,16 +111,33 @@ function _create_company(company_def, conn, all_done) {
         });
       },
       (done) => {
-        const sql = "INSERT IGNORE INTO company (name, industry_id, email_domain, property_bag) VALUES (?, ?, ?, ?)";
-        const values = [company_def.name, industry_id, company_def.email_domain, JSON.stringify(company_def.property_bag)];
+        const sql = "SELECT company_id FROM company WHERE name = ?";
+        const values = [company_def.name];
         db.queryWithConnection(conn, sql , values, (error, results) => {
           if(error) {
             console.error("_create_company SQL error: " + error);
-          } else {
-            company_id = results.insertId;
+          } else if(results.length > 0) {
+            company_id = results[0].company_id;
           }
-          done(error, company_id);
+          done(error);
         });
+      },
+      (done) => {
+        if(!company_id) {
+          const sql = "INSERT INTO company (name, industry_id, email_domain, property_bag) VALUES (?, ?, ?, ?)";
+          const values = [company_def.name, industry_id, company_def.email_domain, JSON.stringify(company_def.property_bag)];
+          db.queryWithConnection(conn, sql , values, (error, results) => {
+            if(error) {
+              block_err = error;
+              console.error("_create_company SQL error: " + error);
+            } else {
+              company_id = results.insertId;
+            }
+            done(error);
+          });
+        } else {
+          done();
+        }
       }
     ],
     (error) => {
@@ -141,56 +145,140 @@ function _create_company(company_def, conn, all_done) {
     });
   }
 }
-function _create_job_role(role_def, conn, done) {
+function _create_job_role(role_def, conn, all_done) {
   if(role_def.id) {
-    done(null, role_def.id);
+    all_done(null, role_def.id);
   } else {
-    const sql = "INSERT IGNORE INTO job_role (job_role_name, job_role_descr) VALUES (?, ?)";
-    const values = [role_def.name, role_def.type];
-    db.queryWithConnection(conn, sql, values, (error, results) => {
-      let role_id;
-      if(error) {
-        console.error("_create_job_role SQL error: " + error);
-      } else {
-        role_id = results.insertId;
+    let role_id
+    async.series([
+      (done) => {
+        const sql = "SELECT job_role_id FROM job_role WHERE job_role_descr = ?";
+        const values = [role_def.type];
+        db.queryWithConnection(conn, sql , values, (error, results) => {
+          if(error) {
+            console.error("_create_job_role SQL error: " + error);
+          } else if(results.length > 0) {
+            role_id = results[0].job_role_id;
+          }
+          done(error);
+        });
+      },
+      (done) => {
+        if(!role_id) {
+          const sql = "INSERT IGNORE INTO job_role (job_role_name, job_role_descr) VALUES (?, ?)";
+          const values = [role_def.name, role_def.type];
+          db.queryWithConnection(conn, sql, values, (error, results) => {
+            if(error) {
+              block_err = error;
+              console.error("_create_job_role SQL error: " + error);
+            } else {
+              role_id = results.insertId;
+            }
+            done(error);
+          });
+        } else {
+          done();
+        }
       }
-      done(error, role_id);
+    ],
+    (error) => {
+      all_done(error, role_id);
     });
   }
 }
-function _create_job_type(type_def, conn, done) {
+function _create_job_type(type_def, conn, all_done) {
   if(type_def.id) {
-    done(null, type_def.id);
+    all_done(null, type_def.id);
   } else {
-    const sql = "INSERT IGNORE INTO job_type (job_type_name, job_type_descr) VALUES (?, ?)";
-    const values = [type_def.name, type_def.type];
-    db.queryWithConnection(conn, sql, values, (error, results) => {
-      let type_id;
-      if(error) {
-        console.error("_create_job_type SQL error: " + error);
-      } else {
-        type_id = results.insertId;
-      }
-      done(error, type_id);
+    let type_id;
+    async.series([
+      (done) => {
+        const sql = "SELECT job_type_id FROM job_type WHERE job_type_descr = ?";
+        const values = [type_def.type];
+        db.queryWithConnection(conn, sql , values, (error, results) => {
+          if(error) {
+            console.error("_create_job_type SQL error: " + error);
+          } else if(results.length > 0) {
+            type_id = results[0].job_type_id;
+          }
+          done(error);
+        });
+      },
+      (done) => {
+        if(!type_id) {
+          const sql = "INSERT IGNORE INTO job_type (job_type_name, job_type_descr) VALUES (?, ?)";
+          const values = [type_def.name, type_def.type];
+          db.queryWithConnection(conn, sql, values, (error, results) => {
+            if(error) {
+              console.error("_create_job_type SQL error: " + error);
+              block_err = error;
+            } else {
+              type_id = results.insertId;
+            }
+            done(error);
+          });
+        } else {
+          done();
+        }
+      },
+    ],
+    (error) => {
+      all_done(error, type_id);
     });
   }
 }
-function _create_industry(industry_def, conn, done) {
+function _create_industry(industry_def, conn, all_done) {
   if(industry_def.id) {
-    done(null, industry_def.id);
+    all_done(null, industry_def.id);
   } else {
-    const sql = "INSERT IGNORE INTO industry (industry_name, industry_type) VALUES (?, ?)";
-    const values = [industry_def.name, industry_def.type];
-    db.queryWithConnection(conn, sql, values, (error, results) => {
-      let industry_id;
-      if(error) {
-        console.error("_create_company SQL error: " + error);
-      } else {
-        industry_id = results.insertId;
-      }
-      done(error, industry_id);
+    let industry_id;
+    async.series([
+      (done) => {
+        const sql = "SELECT industry_id FROM industry WHERE industry_type = ?";
+        const values = [industry_def.type];
+        db.queryWithConnection(conn, sql , values, (error, results) => {
+          if(error) {
+            console.error("_create_industry SQL error: " + error);
+          } else if(results.length > 0) {
+            industry_id = results[0].industry_id;
+          }
+          done(error);
+        });
+      },
+      (done) => {
+        let block_err;
+        if(!industry_id) {
+          const sql = "INSERT IGNORE INTO industry (industry_name, industry_type) VALUES (?, ?)";
+          const values = [industry_def.name, industry_def.type];
+          db.queryWithConnection(conn, sql, values, (error, results) => {
+            if(error) {
+              console.error("_create_company SQL error: " + error);
+              block_err = error;
+            } else {
+              industry_id = results.insertId;
+            }
+          });
+        }
+        done(block_err);
+      },
+    ],
+    (error) => {
+      all_done(error, industry_id);
     });
   }
+}
+function _extract_job_def(req) {
+  return {
+    title: req.body.title,
+    location: req.body.location || null,
+    pay_rate_min: req.body.pay_rate_min || null,
+    pay_rate_max: req.body.pay_rate_max || null,
+    min_gpa: req.body.min_gpa || null,
+    description: req.body.description || null,
+    external_url: req.body.external_url || null,
+    posted_at: req.body.posted_at || null,
+    takedown_at: req.body.takedown_at || null
+  };
 }
 function _make_job_from_results(results) {
   let result = {};
@@ -225,65 +313,13 @@ function _radius_lat_long_calc(lat, lon, radius) {
 
   return [x1, y1, x2, y2];
 }
-function _to_radians(degrees) {
-  return degrees * Math.PI / 180;
-}
 function _to_degrees(radians) {
   return radians * 180 / Math.PI;
 }
-
-function get_jobs(req, res) {
-  const search_location = req.body.location || req.query.location;
-  const search_radius_label = req.body.radius || req.query.radius;
-  let search_lat;
-  let search_long;
-  let search_formatted;
-
-  async.series([
-    (done) => {
-      geocoder.geocode(search_location)
-        .then((res) => {
-          search_formatted = res[0].formattedAddress;
-          search_lat = res[0].latitude;
-          search_long = res[0].longitude;
-          done();
-        })
-        .catch((err) => {
-          console.error(err);
-          done(err);
-        });
-    },
-    (done) => {
-      const values = [search_lat, search_lat, search_long, search_long];
-      const sql = "SELECT job.*, company.* , industry.*, job_role.*, job_type.*, job_skill.*, skill_type.* " +
-                  "FROM job " +
-                  "JOIN company USING(company_id) " +
-                  "JOIN industry USING(industry_id) " +
-                  "JOIN job_role USING(job_role_id) " +
-                  "JOIN job_type USING(job_type_id) " +
-                  "LEFT JOIN job_skill USING(job_id) " +
-                  "LEFT JOIN skill_type ON job_skill.skill_type_id = skill_type.skill_type_id " +
-                  "WHERE " +
-                  "latitude_lower_"  + search_radius_label + " <= ? AND " +
-                  "latitude_upper_"  + search_radius_label + " >= ? AND " +
-                  "longitude_lower_" + search_radius_label + " >= ? AND " +
-                  "longitude_upper_" + search_radius_label + " <= ?";
-
-      db.connectAndQuery({sql, values}, (error, results) => {
-        if(error) {
-          console.error(error);
-          res.sendStatus(500);
-        } else {
-          let result = _make_job_from_results(results);
-          res.status(200).send(result);
-        }
-      });
-    },
-  ],
-  (error) => {
-    res.sendStatus(500);
-  });
+function _to_radians(degrees) {
+  return degrees * Math.PI / 180;
 }
+
 function create_job(req, res) {
   const company_def = req.body.company;
   const job_role = req.body.job_role;
@@ -417,33 +453,6 @@ function create_job(req, res) {
     }
   });
 }
-function update_job(req, res) {
-  const company_def = req.body.company;
-  const job_role = req.body.job_role;
-  const job_type = req.body.job_type;
-  const job_values = _extract_job_def(req);
-
-  if(!company_def.id || !job_role.id || !job_type.id) {
-    res.status(400).send("When updating a job, company, job roles, and job types cannot be created.");
-  } else {
-    const args = [];
-    const values = [];
-    _.each(Object.keys(job_values), (column) => {
-      args.push(column + "=?");
-      values.push(job_values[column]);
-    });
-
-    const sql = "UPDATE job SET " + args.join(",");
-    db.connectAndQuery({sql, values}, (error, results) => {
-      if(error) {
-        console.error("update_job SQL error: " + error);
-        res.sendStatus(500);
-      } else {
-        res.sendStatus(200);
-      }
-    });
-  }
-}
 function delete_job(req, res) {
   const values = [req.params.job_id];
   const sql = "DELETE FROM job WHERE job_id = $1::int";
@@ -475,4 +484,83 @@ function get_job(req, res) {
         res.status(200).send(result);
       }
     });
+}
+function get_jobs(req, res) {
+  const search_location = req.body.location || req.query.location;
+  const search_radius_label = req.body.radius || req.query.radius;
+  let search_lat;
+  let search_long;
+  let search_formatted;
+
+  async.series([
+    (done) => {
+      geocoder.geocode(search_location)
+        .then((res) => {
+          search_formatted = res[0].formattedAddress;
+          search_lat = res[0].latitude;
+          search_long = res[0].longitude;
+          done();
+        })
+        .catch((err) => {
+          console.error(err);
+          done(err);
+        });
+    },
+    (done) => {
+      const values = [search_lat, search_lat, search_long, search_long];
+      const sql = "SELECT job.*, company.* , industry.*, job_role.*, job_type.*, job_skill.*, skill_type.* " +
+                  "FROM job " +
+                  "JOIN company USING(company_id) " +
+                  "JOIN industry USING(industry_id) " +
+                  "JOIN job_role USING(job_role_id) " +
+                  "JOIN job_type USING(job_type_id) " +
+                  "LEFT JOIN job_skill USING(job_id) " +
+                  "LEFT JOIN skill_type ON job_skill.skill_type_id = skill_type.skill_type_id " +
+                  "WHERE " +
+                  "latitude_lower_"  + search_radius_label + " <= ? AND " +
+                  "latitude_upper_"  + search_radius_label + " >= ? AND " +
+                  "longitude_lower_" + search_radius_label + " >= ? AND " +
+                  "longitude_upper_" + search_radius_label + " <= ?";
+
+      db.connectAndQuery({sql, values}, (error, results) => {
+        if(error) {
+          console.error(error);
+          res.sendStatus(500);
+        } else {
+          let result = _make_job_from_results(results);
+          res.status(200).send(result);
+        }
+      });
+    },
+  ],
+  (error) => {
+    res.sendStatus(500);
+  });
+}
+function update_job(req, res) {
+  const company_def = req.body.company;
+  const job_role = req.body.job_role;
+  const job_type = req.body.job_type;
+  const job_values = _extract_job_def(req);
+
+  if(!company_def.id || !job_role.id || !job_type.id) {
+    res.status(400).send("When updating a job, company, job roles, and job types cannot be created.");
+  } else {
+    const args = [];
+    const values = [];
+    _.each(Object.keys(job_values), (column) => {
+      args.push(column + "=?");
+      values.push(job_values[column]);
+    });
+
+    const sql = "UPDATE job SET " + args.join(",");
+    db.connectAndQuery({sql, values}, (error, results) => {
+      if(error) {
+        console.error("update_job SQL error: " + error);
+        res.sendStatus(500);
+      } else {
+        res.sendStatus(200);
+      }
+    });
+  }
 }
